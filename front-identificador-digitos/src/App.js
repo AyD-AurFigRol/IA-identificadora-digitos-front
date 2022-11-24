2eb5b286a939317b30ee59a40555851912d25bcd
DanAurMG <raizeremi.15.damg@gmail.com>
2022-11-24 09:25:53 -0600

    DAMG 24-11-2022: Instale algunas deoendencias y modifique el app.js

--- front-identificador-digitos/src/App.js
+++ front-identificador-digitos/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Esto es el header de la que deberia ser la landing, meterle react router para mandar a la landing de verdad</p>
       </header>
+      <div className = "container">
+        <p>Primer texto de esta aplicacion</p>
+      </div>
     </div>
   );
 }
-
-export default App;
